c9b1e1c5861b283198997b22b4973b9807997246
Joeldeep Kaur <33649029+JFA17SCM04K@users.noreply.github.com>
2018-02-20 19:16:02 -0600

    Models added

---
+++ models/campgrounds.js
@@ -0,0 +1,26 @@
+var mongoose=require("mongoose");
+
+var campgroundSchema=new mongoose.Schema({
+
+    name: String,
+    image: String,
+    price: String,
+    location: String,
+    lat: Number,
+    lng: Number,
+    description: String,
+    createdAt: { type: Date, default: Date.now },
+    author:{
+        id:{
+            type:mongoose.Schema.Types.ObjectId,
+            ref:"User"
+            },
+        username: String
+     },
+    comments:[{
+        type:mongoose.Schema.Types.ObjectId,
+        ref:"Comment"
+    }]
+}, { usePushEach: true });
+
+module.exports=mongoose.model("Campground", campgroundSchema);
